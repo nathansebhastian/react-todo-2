55aed4a4fbc00d754df57a355806456c6516d0cd
Nathan Sebhastian <nathansebhastian@gmail.com>
2024-01-02 20:51:54 +0700

    list and add task OK

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react';
 import { FaCheckSquare } from 'react-icons/fa';
-import { BsCheck2Square } from 'react-icons/bs';
-import { TbRefresh } from 'react-icons/tb';
-import { FaRegEdit } from 'react-icons/fa';
-import { RiDeleteBin7Line } from 'react-icons/ri';
+import { AddTask, TaskList } from './components';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -21,6 +19,7 @@ function App() {
 
   return (
     <>
+      <Toaster position='bottom-right' />
       {/* Task Title */}
       <div className='max-w-lg px-5 m-auto mt-20'>
         <h1 className='flex justify-center text-3xl font-bold underline'>
@@ -28,72 +27,8 @@ function App() {
           <FaCheckSquare style={{ color: '#42C239' }} />
         </h1>
       </div>
-      {/* Add Task */}
-      <form>
-        <div className='flex items-center w-full max-w-lg gap-2 p-5 m-auto'>
-          <input
-            type='text'
-            placeholder='start typing ...'
-            className='w-full px-5 py-2 bg-transparent border-2 outline-none border-zinc-600 rounded-xl placeholder:text-zinc-500 focus:border-sky-700'
-          />
-          <button
-            type='submit'
-            className='px-5 py-2 text-md text-white bg-blue-500 border-2 border-transparent rounded-lg hover:bg-blue-700'
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-      {/* Task List */}
-      <ul className='grid max-w-lg gap-2 px-5 m-auto'>
-        {tasks.map(task => (
-          <li
-            key={task.id}
-            className={`p-5 rounded-xl bg-zinc-200 ${
-              task.completed === 'completed' && 'bg-opacity-50 text-zinc-500'
-            }`}
-          >
-            <div className='flex flex-col gap-5'>
-              <span
-                style={{
-                  textDecoration: task.completed ? 'line-through' : 'none',
-                }}
-              >
-                {task.title}
-              </span>
-              <div className='flex justify-between gap-5'>
-                <button>
-                  {task.completed ? (
-                    <span className='flex items-center gap-1'>
-                      <TbRefresh />
-                      Set Active
-                    </span>
-                  ) : (
-                    <span className='flex items-center gap-1'>
-                      <BsCheck2Square />
-                      Set Completed
-                    </span>
-                  )}
-                </button>
-                <div className='flex items-center gap-2'>
-                  <button
-                    className='flex items-center gap-1 '
-                  >
-                    <FaRegEdit />
-                    Edit
-                  </button>
-                  <button
-                    className='flex items-center gap-1 text-red-500'
-                  >
-                    <RiDeleteBin7Line />
-                    Delete
-                  </button>
-                </div>
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <AddTask setTasks={setTasks} />
+      <TaskList tasks={tasks} />
     </>
   );
 }
---
+++ src/components/AddTask.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useRef, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { nanoid } from 'nanoid';
+
+
+export const AddTask = ({setTasks}) => {
+
+  const [title, setTitle] = useState('');
+
+  const addTask = title => {
+    const newTask = {
+      title: title,
+      completed: false,
+      id: nanoid()
+    };
+
+    setTasks(tasks => [...tasks, newTask])
+    setTitle('')
+    toast.success('New Task added!')
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (title.trim() !== '') {
+      addTask(title);
+    } else {
+      toast.error('Task field cannot be empty!');
+    }
+  };
+
+  return (
+    <form>
+      <div className='flex items-center w-full max-w-lg gap-2 p-5 m-auto'>
+        <input
+          type='text'
+          placeholder='start typing ...'
+          className='w-full px-5 py-2 bg-transparent border-2 outline-none border-zinc-200 rounded-xl placeholder:text-zinc-500 focus:border-zinc-700'
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+        />
+        <button
+          onClick={handleSubmit}
+          type='submit'
+          className='px-5 py-2 text-md text-white bg-blue-500 border-2 border-transparent rounded-lg hover:bg-blue-700'
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+};
